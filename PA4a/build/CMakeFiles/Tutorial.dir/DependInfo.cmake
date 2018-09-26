39faac533f89a2683144c3d435a2d90d317c5d31
Christina Symon <christinasymon@Christinas-MacBook-Pro.local>
2018-09-25 22:04:15 -0700

    Pa4 model loading

---
+++ PA4a/build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/christinasymon/cs680Symon/pa4a/src/camera.cpp" "/Users/christinasymon/cs680Symon/pa4a/build/CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "/Users/christinasymon/cs680Symon/pa4a/src/engine.cpp" "/Users/christinasymon/cs680Symon/pa4a/build/CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "/Users/christinasymon/cs680Symon/pa4a/src/graphics.cpp" "/Users/christinasymon/cs680Symon/pa4a/build/CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "/Users/christinasymon/cs680Symon/pa4a/src/main.cpp" "/Users/christinasymon/cs680Symon/pa4a/build/CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "/Users/christinasymon/cs680Symon/pa4a/src/object.cpp" "/Users/christinasymon/cs680Symon/pa4a/build/CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "/Users/christinasymon/cs680Symon/pa4a/src/shader.cpp" "/Users/christinasymon/cs680Symon/pa4a/build/CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "/Users/christinasymon/cs680Symon/pa4a/src/window.cpp" "/Users/christinasymon/cs680Symon/pa4a/build/CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNIX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include/SDL2"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
